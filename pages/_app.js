2678912e7d0759f8f3306777e30a2e5fdedc6769
Nitish Jha <nitishkumar0914@gmail.com>
2022-04-04 15:17:11 +0530

    tyb

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
-import Header from '../Components/Header'
-import Footer from '../Components/Footer'
+import Header from '../public/components/header'
+import Footer from '../public/components/footer'
 
 function MyApp({ Component, pageProps }) {
   return <> 
