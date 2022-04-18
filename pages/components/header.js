e646f11cd8a1a9551c506dadb53243a311583939
Nitish Jha <nitishkumar0914@gmail.com>
2022-04-18 23:58:48 +0530

    Update header.js

--- pages/components/header.js
+++ pages/components/header.js
@@ -2,30 +2,30 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRef } from 'react'
-import{AiOutlineShoppingCart} from 'react-icons/ai'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 
 
 const header = () => {
-//     const toggleNav = () => {
-//         if (ref.current.classList.contains('hidden')) {
-//             ref.current.classList.remove('hidden')
-//             ref.current.classList.add('block')
-//         }
-//         else if (!ref.current.classList.contains('hidden')) {
-//             ref.current.classList.remove('block')
-//             ref.current.classList.add('hidden')
-//         }     
+    //     const toggleNav = () => {
+    //         if (ref.current.classList.contains('hidden')) {
+    //             ref.current.classList.remove('hidden')
+    //             ref.current.classList.add('block')
+    //         }
+    //         else if (!ref.current.classList.contains('hidden')) {
+    //             ref.current.classList.remove('block')
+    //             ref.current.classList.add('hidden')
+    //         }     
 
-//     }
-//     const ref = useRef()
-    
+    //     }
+    //     const ref = useRef()
+
     return (
         <div className='sticky top-0 z-50 bg-slate-50'>
             <nav className="flex flex-col justify-between items-center md:flex-row text-slate-800 border-2 drop-shadow-lg rounded-2xl">
                 <Link href="/"><a className="mx-4" ><Image className="border-2 rounded-xl" src="/sk-computer-logo.jpg" alt="Logo sk-computer saharsa" width={45} height={45} /></a></Link>
 
                 {/* ref = {ref} */}
-                <ul  className="flex flex-wrap md:mr-10" id="navmenu">
+                <ul className="flex flex-wrap md:mr-10" id="navmenu">
                     <li className="md:m-6 m-3 "> <Link href='/' ><a
                         className="p-2 rounded-xl cursor-pointer shadow-md bg-slate-50 hover:bg-slate-100 hover:drop-shadow-lg"
                         title="Home">Home</a></Link></li>
@@ -38,19 +38,21 @@ const header = () => {
                     <li className="md:m-6 m-3 "><Link href='/services' ><a
                         className="p-2 rounded-xl cursor-pointer shadow-md bg-slate-50 hover:bg-slate-100 hover:drop-shadow-lg"
                         title="Services offered">Services</a></Link></li>
-            
+
                 </ul>
-                <div className='absolute right-6 top-4 text-3xl md:right-3 md:top-5 md:text-3xl'>
-                <AiOutlineShoppingCart/>
+                <div className='absolute right-6 top-4 cursor-pointer text-3xl md:right-3 md:top-5 md:text-3xl'>
+                    <a title='Your Shopping cart' className=''><AiOutlineShoppingCart /></a>
                 </div>
                 {/* onClick={toggleNav} */}
-                <a href="#"  className="md:hidden absolute left-6 top-4">
+                <a href="#" className="md:hidden absolute left-6 top-4">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
-
                 </a>
+                <div className='hidden absolute right-0 h-full bg-gray-400'>
+                    printer Repair
+                </div>
             </nav>
 
         </div>
